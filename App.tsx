47a2894443251647d592483850a91aafc18dddb1
Mikael Siidorow <mikael.siidorow@aalto.fi>
2022-11-05 00:19:34 +0200

    Initial commit, working firebase

--- App.tsx
+++ App.tsx
@@ -1,10 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { db } from "./config";
+import { collection, getDocs, setDoc, doc } from "@firebase/firestore";
 
 export default function App() {
+  const [text, setText] = React.useState("");
+  useEffect(() => {
+    const getData = async () => {
+      const testCol = collection(db, "/test");
+      const testSnapshot = await getDocs(testCol);
+      const testList = testSnapshot.docs.map((doc) => doc.data());
+      setText(testList[testList.length - 1].name);
+    };
+    const setData = async () => {
+      const testCol = collection(db, "/test");
+      await setDoc(doc(testCol, "test"), {
+        name: "test",
+      });
+    };
+    setData();
+    getData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{text}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ config/firebase.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import Constants from "expo-constants";
+
+// Optionally import the services that you want to use
+//import {...} from "firebase/auth";
+//import {...} from "firebase/database";
+//import {...} from "firebase/firestore";
+//import {...} from "firebase/functions";
+//import {...} from "firebase/storage";
+
+// Initialize Firebase
+export const firebaseConfig = {
+  apiKey: Constants.manifest.extra.apiKey,
+  authDomain: Constants.manifest.extra.authDomain,
+  projectId: Constants.manifest.extra.projectId,
+  storageBucket: Constants.manifest.extra.storageBucket,
+  messagingSenderId: Constants.manifest.extra.messagingSenderId,
+  appId: Constants.manifest.extra.appId,
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
