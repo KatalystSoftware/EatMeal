e71b7abdc68c911818c85e0da6aff3e3747ca5ce
Mikael Siidorow <87767032+MikaelSiidorow@users.noreply.github.com>
2022-11-06 00:34:26 +0200

    Update styles, remove bottomnav from camera (#1)
    * Update styles, remove bottomnav from camera
    
    * fix merge

--- screens/UploadScreen.tsx
+++ screens/UploadScreen.tsx
@@ -15,71 +15,77 @@ import { Camera, CameraCapturedPicture, CameraType } from "expo-camera";
 import MaterialIcon from "react-native-vector-icons/MaterialIcons";
 import { storage, db } from "../config";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { collection, addDoc, Timestamp, updateDoc, doc } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  Timestamp,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
 import { AuthContext, PostContext } from "../context";
 import { Category } from "../types";
 import { manipulateAsync, SaveFormat } from "expo-image-manipulator";
 import { Picker } from "@react-native-picker/picker";
+import type { BottomTabBarProps } from "@react-navigation/bottom-tabs";
+import { useIsFocused, CommonActions } from "@react-navigation/native";
+import { firebaseConfig } from "../config/firebase";
 
 type Steps = "camera" | "preview" | "info" | "uploading";
 
-import { firebaseConfig } from '../config/firebase'
-
-const getLabelsFromImage = async (uri : string) : (Promise<string[] | null>) => {
-  try {
-    console.log('Requesting object labels and rectangles for uri ', uri)
-    let response = await fetch(
-      'https://vision.googleapis.com/v1/images:annotate?key=' +
-      firebaseConfig.apiKey,
-      {
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
+const UploadScreen = ({ navigation }: BottomTabBarProps) => {
+  const getLabelsFromImage = async (uri: string): Promise<string[] | null> => {
+    try {
+      console.log("Requesting object labels and rectangles for uri ", uri);
+      let response = await fetch(
+        "https://vision.googleapis.com/v1/images:annotate?key=" +
+          firebaseConfig.apiKey,
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+          body: JSON.stringify({
+            requests: [
+              {
+                features: [{ type: "OBJECT_LOCALIZATION", maxResults: 10 }],
+                image: {
+                  source: {
+                    imageUri: uri,
+                  },
+                },
+              },
+            ],
+          }),
         },
-        method: 'POST',
-        body: JSON.stringify({
-          requests: [
-            {
-              features: [
-                { type: 'OBJECT_LOCALIZATION', maxResults: 10 }
-              ],
-              image: {
-                source: {
-                  imageUri: uri
-                }
-              }
-            }
-          ]
-        })
+      );
+
+      let responseJson = await response.json();
+      console.log("google responded", JSON.stringify(responseJson));
+
+      if (
+        responseJson.responses &&
+        responseJson.responses[0] &&
+        responseJson.responses[0].localizedObjectAnnotations
+      ) {
+        // get all labels
+        const labels = responseJson.responses[0].localizedObjectAnnotations.map(
+          (label: any) => label.name,
+        );
+
+        console.log("labels", labels);
+
+        return labels;
+      } else {
+        console.log("No object labels found");
       }
-    )
-
-    let responseJson = await response.json();
-    console.log('google responded', JSON.stringify(responseJson))
-    
-    if (
-      responseJson.responses && responseJson.responses[0] && responseJson.responses[0].localizedObjectAnnotations
-    ) {
-      // get all labels
-      const labels = responseJson.responses[0].localizedObjectAnnotations.map(
-        (label: any) => label.name
-      )
-
-      console.log('labels', labels)
-
-      return labels
-    } else {
-      console.log('No object labels found')
+    } catch (error) {
+      console.log(error);
     }
 
-  } catch (error) {
-     console.log(error);
-  }
+    return null;
+  };
 
-  return null
-}
-
-const UploadScreen = () => {
   const [permission, requestPermission] = Camera.useCameraPermissions();
   const [camera, setCamera] = React.useState<Camera | null>(null);
   const [photo, setPhoto] = React.useState<CameraCapturedPicture | null>(null);
@@ -89,6 +95,7 @@ const UploadScreen = () => {
   const authContext = React.useContext(AuthContext);
   const { user } = authContext.state;
   const { dispatch } = React.useContext(PostContext);
+  const isFocused = useIsFocused();
 
   React.useEffect(() => {
     requestPermission();
@@ -115,6 +122,13 @@ const UploadScreen = () => {
 
   const submitPhoto = () => {
     setStep("info");
+  };
+
+  const goBack = () => {
+    setPhoto(null);
+    setCaption("");
+    setStep("camera");
+    navigation.dispatch(CommonActions.goBack());
   };
 
   const post = async () => {
@@ -165,13 +179,12 @@ const UploadScreen = () => {
       setCaption("");
       setStep("camera");
 
-      let labels = await getLabelsFromImage(downloadURL)
+      let labels = await getLabelsFromImage(downloadURL);
 
       // add labels to newPost
       await updateDoc(doc(db, "posts", docRef.id), {
-        labels: labels
-      })
-
+        labels: labels,
+      });
     }
   };
 
@@ -182,17 +195,36 @@ const UploadScreen = () => {
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
       <View style={styles.container}>
-        {step === "camera" && (
+        {step === "camera" && isFocused && (
           <Camera
             ref={ref => setCamera(ref)}
             style={styles.camera}
             type={CameraType.back}
           >
-            <Pressable onPress={takePhoto}>
-              <View style={styles.photoButtonContainer}>
-                <MaterialIcon size={56} color="#fff" name="camera" />
-              </View>
-            </Pressable>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Pressable onPress={takePhoto}>
+                <View style={styles.photoButtonContainer}>
+                  <MaterialIcon size={56} color="#fff" name="camera" />
+                </View>
+              </Pressable>
+              <Pressable
+                style={{
+                  position: "absolute",
+                  transform: [{ translateX: 65 }],
+                }}
+                onPress={goBack}
+              >
+                <View style={styles.cancelButtonContainer}>
+                  <MaterialIcon size={36} color="#f00" name="cancel" />
+                </View>
+              </Pressable>
+            </View>
           </Camera>
         )}
         {step === "preview" && photo && (
@@ -205,7 +237,6 @@ const UploadScreen = () => {
               >
                 <Pressable onPress={retakePhoto}>
                   <View style={styles.cancelButtonContainer}>
-                    <Text style={styles.buttonText}>Retake</Text>
                     <MaterialIcon size={36} color="#f00" name="cancel" />
                   </View>
                 </Pressable>
@@ -215,7 +246,7 @@ const UploadScreen = () => {
               <Pressable onPress={submitPhoto}>
                 <View style={styles.postButtonContainer}>
                   <Text style={styles.buttonText}>Continue</Text>
-                  <MaterialIcon size={36} color="#000" name="arrow-right" />
+                  <MaterialIcon size={36} color="#fff" name="arrow-right" />
                 </View>
               </Pressable>
             </View>
@@ -230,36 +261,44 @@ const UploadScreen = () => {
               >
                 <Pressable onPress={retakePhoto}>
                   <View style={styles.cancelButtonContainer}>
-                    <Text style={styles.buttonText}>Retake</Text>
                     <MaterialIcon size={36} color="#f00" name="cancel" />
                   </View>
                 </Pressable>
               </ImageBackground>
             </View>
             <View style={styles.previewBottomContainer}>
-              <Picker
-                style={{ width: "50%" }}
-                prompt="Select Category"
-                selectedValue={category}
-                onValueChange={(value, _) => setCategory(value)}
-              >
-                <Picker.Item label="Breakfast" value={Category.Breakfast} />
-                <Picker.Item label="Lunch" value={Category.Lunch} />
-                <Picker.Item label="Dinner" value={Category.Dinner} />
-                <Picker.Item label="Snack" value={Category.Snack} />
-              </Picker>
-              <TextInput
-                placeholder="Write a caption..."
-                maxLength={120}
-                style={{
-                  padding: 10,
-                  fontSize: 18,
-                  width: "100%",
-                  textAlign: "center",
-                }}
-                onChangeText={t => setCaption(t)}
-                value={caption}
-              />
+              <View style={{ width: "100%", flexDirection: "row" }}>
+                <TextInput
+                  multiline
+                  numberOfLines={4}
+                  placeholder="Write a caption..."
+                  maxLength={120}
+                  style={{
+                    padding: 10,
+                    fontSize: 18,
+                    width: "50%",
+                    textAlign: "center",
+                    backgroundColor: "#ddd",
+                  }}
+                  onChangeText={t => setCaption(t)}
+                  value={caption}
+                />
+                <Picker
+                  style={{
+                    width: "50%",
+                    backgroundColor: "#ddd",
+                  }}
+                  prompt="Select Category"
+                  selectedValue={category}
+                  onValueChange={(value, _) => setCategory(value)}
+                >
+                  <Picker.Item label="Breakfast" value={Category.Breakfast} />
+                  <Picker.Item label="Lunch" value={Category.Lunch} />
+                  <Picker.Item label="Dinner" value={Category.Dinner} />
+                  <Picker.Item label="Snack" value={Category.Snack} />
+                </Picker>
+              </View>
+
               <Pressable disabled={step === "uploading"} onPress={post}>
                 <View style={styles.postButtonContainer}>
                   <Text style={styles.buttonText}>Post your RealMeal!</Text>
@@ -309,14 +348,18 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
     paddingVertical: 10,
     alignItems: "center",
-    backgroundColor: "#ddd",
+    backgroundColor: "#af52de",
     borderRadius: 25,
     marginBottom: 5,
   },
   photoButtonContainer: {
     alignItems: "center",
+    backgroundColor: "#af52de",
+    padding: 4,
+    margin: 8,
+    borderRadius: 50,
   },
-  buttonText: { fontSize: 25, fontWeight: "bold" },
+  buttonText: { fontSize: 25, fontWeight: "bold", color: "#fff" },
 
   takeButtonText: {},
 });
